2ca83b3cf5c12779051ee0614549b423c94c3afc
florent-le-maire <lemaireflorent9@gmail.com>
2021-02-09 07:19:02 -0800

    first commit

---
+++ lib/tp_rest_train.dart
@@ -0,0 +1,11 @@
+/// tp_rest_train
+///
+/// A Aqueduct web server.
+library tp_rest_train;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
